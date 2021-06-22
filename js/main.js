2ddf96542fc7b5ef16a98d3db6c8f51ffc32260a
justusbersten <justusbersten@gmail.com>
2021-06-22 07:40:21 +0200

    add: main js file with charts

---
+++ js/main.js
@@ -0,0 +1,42 @@
+import * as Utils from "./utils.js";
+
+
+var ctx = document.getElementById('meanWeekday').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag'],
+        datasets: [{
+            label: 'Durchschnittliche Anzahl Besucher je Wochentag',
+            data: [12, 19, 3, 5, 30, 50, 45],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+
+const labels = Utils.months({count:7});
+console.log(labels);
