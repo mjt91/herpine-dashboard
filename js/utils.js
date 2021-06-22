35032ecd177a5e99075183d6d94cb56590a58d7b
justusbersten <justusbersten@gmail.com>
2021-06-22 07:39:21 +0200

    add: utils for time series charts (MONTHS)

---
+++ js/utils.js
@@ -0,0 +1,31 @@
+// Adapted from http://indiegamr.com/generate-repeatable-random-numbers-in-js/
+
+const MONTHS = [
+  'Januar',
+  'Februar',
+  'März',
+  'April',
+  'Mai',
+  'Juni',
+  'Juli',
+  'August',
+  'September',
+  'Oktober',
+  'November',
+  'Dezember'
+];
+
+export function months(config) {
+  var cfg = config || {};
+  var count = cfg.count || 12;
+  var section = cfg.section;
+  var values = [];
+  var i, value;
+
+  for (i = 0; i < count; ++i) {
+    value = MONTHS[Math.ceil(i) % 12];
+    values.push(value.substring(0, section));
+  }
+
+  return values;
+}
