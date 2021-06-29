e9ef2987f7dada7e5245519008507c76e66b7498
justusbersten <justusbersten@gmail.com>
2021-06-29 15:07:43 +0200

    refactor: weekday barchart structure

--- js/main.js
+++ js/main.js
@@ -79,41 +79,68 @@ import * as Utils from "./utils.js";
 })();
 
 (function() {
-    const ctxWeekdays = document.getElementById('meanWeekday').getContext('2d');
-    new Chart(ctxWeekdays, {
-        type: 'bar',
-        data: {
-            labels: ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag'],
-            datasets: [{
-                label: 'Durchschnittliche Anzahl Besucher je Wochentag',
-                data: [12, 19, 3, 5, 30, 50, 45],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
+    fetch("./data/herpine_weekdaymean.json")
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(myJson) {
+            // get data from json
+            const weeklyData = [
+                myJson.Montag.GUESTS,
+                myJson.Dienstag.GUESTS,
+                // myJson.Mittwoch.GUESTS,
+                0,
+                // myJson.Freitag.GUESTS,
+                0,
+                // myJson.Donnerstag.GUESTS,
+                0,
+                // myJson.Freitag.GUESTS,
+                0,
+                myJson.Sonntag.GUESTS,
+            ];
+            console.log(weeklyData);
+
+            const data = {
+                labels: ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag'],
+                datasets: [
+                    {
+                        label: 'Durchschnittliche Anzahl Besucher je Wochentag',
+                        data: weeklyData,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }
+                ]
+            };
+            const config = {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
                 }
-            }
-        }
-    });
+            };
+
+            const ctxWeekdays = document.getElementById('meanWeekday').getContext('2d');
+            new Chart(ctxWeekdays, {
+                type: 'bar',
+                data: data,
+                options: config
+            });
+        });
 })();
 
 (function() {
@@ -124,6 +151,7 @@ import * as Utils from "./utils.js";
         .then(function(text) {
             console.log(text);
             const updateTime = text;
-            document.getElementById("update").textContent=Utils.parseISODate(updateTime, "f");
+            document.getElementById("update1").textContent=Utils.parseISODate(updateTime, "f");
+            document.getElementById("update2").textContent=Utils.parseISODate(updateTime, "f");
         })
 })();
